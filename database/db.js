24aaa093741e0e195e2b3ae1825ecc099497fce1
Pratham Jain <jainpratham370@gmail.com>
2024-10-21 11:38:15 +0530

    updated

--- database/db.js
+++ database/db.js
@@ -1,25 +1,5 @@
 import mongoose from 'mongoose';
 
-export const dbConnect = () => {
-  mongoose.connect(process.env.MONGO_URI, {
-    dbName: "Todo",
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 10000 // 10 seconds
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((e) => {
-    console.error("Connection error:", e.message);
-    process.exit(1); // Exit the process if the connection fails
-  });
-
-  // Optionally, listen for successful connection
-  mongoose.connection.on('connected', () => {
-    console.log('Mongoose connected successfully');
-  });
-
-  // Optionally, handle disconnection events
-  mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose disconnected');
-  });
-};
+export const dbConnect=()=>mongoose.connect(process.env.MONGO_URI,{
+    dbName:"Todo",
+}).then(()=>console.log("Connected to MongoDB ")).catch((e)=>console.log(e))
